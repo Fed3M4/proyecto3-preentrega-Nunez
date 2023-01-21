f2a5ffc1adc129fc253102e7690ff26f8dbd0b8e
Federico Nuñez <nunez.fedema@gmail.com>
2023-01-20 21:59:19 -0300

    borrado de codigo comentado

--- js/main.js
+++ js/main.js
@@ -9,117 +9,85 @@ const descripcion = document.getElementById('descrip');
 
 //const IDReclamo = document.getElementById('IDReclamo');
 const nroReclamo = [0]
-const IDReclamo = [];
+let IDReclamo = "";
 const delSector = document.getElementById('delSector');
 const solicitante = document.getElementById('solicitante');
 const info = document.getElementById('info');
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
-    obtenerReclamoNuevo();
+    crearCaso();
 })
 
-const obtenerID = () => {
-    let terminado = false;
-    let ultimoNroReclamo = nroReclamo[nroReclamo.length - 1]
-
-    do {
-        const sectorSwitch = sector.value
-
-        switch (sectorSwitch) {
-            case 'Recargas':
-                ultimoNroReclamo+=1;
-                IDReclamo.push("R" + ultimoNroReclamo);
-                console.log(IDReclamo)
-                break;
-            case  'SUBE':
-                numeroReclamo+=1;
-                IDReclamo = 'S' + numeroReclamo;
-                reclamo.push({
-                    id: numeroReclamo,
-                    sector: productoReclama,
-                    motivo: descripcion
-                });
-                alert('Tu número de reclamo es: ' + reclamoS);
-                break;
-            case 'Cobro de servicios':
-                ultimoNroReclamo+=1;
-                IDReclamo = 'CS' + numeroReclamo;
-                reclamo.push({
-                    id: numeroReclamo,
-                    sector: productoReclama,
-                    motivo: descripcion
-                });
-                alert('Tu número de reclamo es: ' + reclamoCS);
-                break;
-            case 'Medios de pago':
-                ultimoNroReclamo+=1;
-                IDReclamo = 'MP' + numeroReclamo;
-                reclamo.push({
-                    id: numeroReclamo,
-                    sector: productoReclama,
-                    motivo: descripcion
-                });
-                alert('Tu número de reclamo es: ' + reclamoMP);
-                break;
-            case 'Transferencias':
-                ultimoNroReclamo+=1;
-                IDReclamo = 'T' + numeroReclamo;
-                reclamo.push({
-                    id: numeroReclamo,
-                    sector: productoReclama,
-                    motivo: descripcion
-                });
-                alert('Tu número de reclamo es: ' + reclamoT);
-                break;
-            case 'Plataforma':
-                ultimoNroReclamo+=1;
-                IDReclamo = 'P' + numeroReclamo;
-                reclamo.push({
-                    id: numeroReclamo,
-                    sector: productoReclama,
-                    motivo: descripcion
-                });
-                alert('Tu número de reclamo es: ' + reclamoP);
-                break;
-            case 'App':
-                ultimoNroReclamo+=1;
-                IDReclamo = 'A' + numeroReclamo;
-                reclamo.push({
-                    id: numeroReclamo,
-                    sector: productoReclama,
-                    motivo: descripcion
-                });
-                alert('Tu número de reclamo es: ' + reclamoA);
-                break;
-            default:
-                alert('No elegiste una opción válida')
-                break;
-        }
-
-        terminado = confirm('¿Querés hacer otro reclamo?')
-    } while (terminado);
-
+function crearCaso() {
+    crearNroSeguimiento(sector)
+    const reclamo1 =  new Reclamos(IDReclamo);
+    obtenerReclamoNuevo(reclamo1)
 }
 
-const obtenerReclamoNuevo = () => {
-    IDReclamo2 = IDReclamo[IDReclamo.length - 1]
-    const reclamo = {
-        IDReclamo: IDReclamo2,
-        sector: sector.value,
-        solicitante: nombre.value + " " + apellido.value,
-        descripcion: descripcion.value
+const crearNroSeguimiento = () => {
+    const section = sector.value
+    const generarID = (section) => {
+        let numerito = nroReclamo[nroReclamo.length - 1] + 1;
+        nroReclamo.push(numerito);
+        return IDReclamo = sector.value.charAt(0) + numerito;
     }
-    pintarReclamo(reclamo)
-
-    guardarReclamoStorage(reclamo)
+    switch (section) {
+        case 'Recargas':
+            generarID();
+            break;
+        case  'SUBE':
+            generarID();
+            break;
+        case 'Cobro de servicios':
+            generarID();
+            break;
+        case 'Medios de pago':
+            generarID();
+            alert('Tu número de reclamo es: ' + reclamoMP);
+            break;
+        case 'Transferencias':
+            generarID();
+            break;
+        case 'Plataforma':
+            generarID();
+            break;
+        case 'App':
+            generarID();
+            break;
+        default:
+            alert('No elegiste una opción válida')
+            break;
+    }
 }
+
+const obtenerReclamoNuevo = (x) => {
+    console.log(x)
+    const reclamo = {
+        IDReclamo: IDReclamo,
+        sector: sector.value,
+        solicitante: x.solicitante,
+        descripcion: descripcion.value
+    };
+
+    const cantR = nroReclamo[nroReclamo.length - 1];
+
+    mostrarQReclamos(cantR);
+
+    pintarReclamo(reclamo);
+
+    guardarReclamoStorage(reclamo);
+};
 
 const pintarReclamo = (reclamo) => {
-    IDReclamo.innerText = `${reclamo.IDReclamo}`;
+    IDpintado.innerText = `${reclamo.IDReclamo}`;
     delSector.innerText = `${reclamo.sector}`;
     solicitante.innerText = `${reclamo.solicitante}`;
     info.innerText = `${reclamo.descripcion}`;
+}
+
+const mostrarQReclamos = (cantR) => {
+    QReclamos.innerText = `${cantR}`
 }
 
 const guardarReclamoStorage = (reclamo) => {
